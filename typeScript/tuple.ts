afb5480bef8bcac0038c0a7639905f45384412b0
sahil <sahil@vidyamantra.com>
2023-03-03 17:57:20 +0530

    solved tuple weird behaviour

--- typeScript/tuple.ts
+++ typeScript/tuple.ts
@@ -1,3 +1,9 @@
-let myTuple:[string,number,number,string] = ["name",23,34,"a"]
+let myTuple:[string,number,number,string?] = ["name",23,34]
 
 myTuple.push(78)
+type obj = {
+roll : string
+}
+let o :obj ={
+    roll: "",
+}
