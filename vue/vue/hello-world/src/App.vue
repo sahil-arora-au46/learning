61878cd545d87aa2128642f5affd0eaea1243be4
sahil <sahil@vidyamantra.com>
2023-03-20 16:07:56 +0530

    initiated vue

---
+++ vue/vue/hello-world/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import home from './components/home.vue'
+</script>
+
+<template>
+ 
+<home  />
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/vue/hello-world/src/components/home.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1> hi from h1</h1>
+</template>
+
+<script>
+export default{
+    name : home
+}
+</script>
