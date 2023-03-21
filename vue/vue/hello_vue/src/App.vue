d81c5df856f43d9f18c24064b130975a91b5d86b
sahil <sahil@vidyamantra.com>
2023-03-21 17:41:42 +0530

    Single-file-component

---
+++ vue/vue/hello_vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<table>
+  <tr>
+    <th v-for="name in names" v-bind:key="name"></th>
+  </tr>
+  <tr v-for="name in names" v-bind:key="name">
+    <td v-for="name in names" v-bind:key="name" v-text="name"></td>
+  </tr>
+</table>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return{
+     names:['a','b','c','d']
+     
+    }
+  }
+}
+</script>
+
+
