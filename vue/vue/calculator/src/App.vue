fee436b70890d0cdd3d600a7f99707a50adf2f58
sahil <sahil@vidyamantra.com>
2023-03-23 17:50:42 +0530

    initiated first project

---
+++ vue/vue/calculator/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+// import { reactive } from 'vue';
+import RefTest from "./components/RefTest.vue"
+// const num = reactive({count:0})
+// setInterval(()=> num.count++,1000                                                                                                                                                                                                                                                     )
+</script>
+<template>
+    <div>
+
+        <RefTest />
+    </div>
+<!-- <hi>{{ num.count }}</hi> -->
+</template>
+<style scoped>
+</style>
---
+++ vue/vue/calculator/src/components/RefTest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from 'vue'
+const num = ref({count:0})
+setInterval(() => {
+    num.value.count++
+}, (1000));
+
+
+</script>
+<template>
+<h1>{{ num }}</h1>
+</template>
+<style scoped>
+
+</style>
